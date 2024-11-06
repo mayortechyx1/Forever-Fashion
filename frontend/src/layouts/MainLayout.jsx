87dc9977c83b8c915ec022e8f2bff08998704571
Iheamarame Emmanuel <princeucboy2017@gmail.com>
2024-11-06 15:13:25 +0100

    Navbar created

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
+import {ToastContainer} from "react-toastify"
+import "react-toastify/ReactToastify.css"
 
 const MainLayout = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
       <Navbar/>
       <Outlet/>
+      <ToastContainer/>
     </div>
   )
 }

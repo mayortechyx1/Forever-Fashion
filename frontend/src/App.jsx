483de37a9dbc04dd6e41fe7b824cca57c51ed1de
Iheamarame Emmanuel <princeucboy2017@gmail.com>
2024-11-06 04:45:13 +0100

    created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PlaceOrder from "./pages/PlaceOrder";
+import Orders from "./pages/Orders";
+import MainLayout from "./layouts/MainLayout";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout/>} >
+          <Route index element={<Home/>} />
+          <Route path="/collection" element={<Collection/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/place-order" element={<PlaceOrder/>} />
+          <Route path="/orders" element={<Orders/>} />
+          <Route path="*" element={<NotFound/>} />
+        </Route>
+    )
+  );
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Navbar"
+
+const MainLayout = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar/>
+      <Outlet/>
+    </div>
+  )
+}
+
+export default MainLayout

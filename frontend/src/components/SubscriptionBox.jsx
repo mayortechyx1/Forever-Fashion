335fb789718b1bf3a243443412816f6e0f181f93
Iheamarame Emmanuel <princeucboy2017@gmail.com>
2024-11-07 02:23:32 +0100

    BestSellers

---
+++ frontend/src/components/SubscriptionBox.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SubscriptionBox = () => {
+  return (
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subscribe now & get $20 off</p>
+      <p className='text-gray-400 mt-3'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+  )
+}
+
+export default SubscriptionBox

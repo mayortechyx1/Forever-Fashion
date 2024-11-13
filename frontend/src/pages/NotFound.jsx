6ed070df8f2c450f0f582ce8fec5da274831c250
Iheamarame Emmanuel <princeucboy2017@gmail.com>
2024-11-13 17:36:34 +0100

    adjusted notFOund

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import Title from "../components/Title";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="border-t pt-16">
+      <div className="text-2xl text-center">
+        <Title text1={"PAGE"} text2={"NOT FOUND"} />
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
